940273fad51cd28b09e13e693e7facc3bd9666ba
dalet <victordalet@protonmail.com>
2023-12-15 18:04:57 +0100

    feat(ser): serv for online mod

---
+++ serv/game-serv/src/positionUser.ts
@@ -0,0 +1,11 @@
+export interface PositionUser {
+    x: number;
+    y: number;
+    user: string;
+}
+
+export interface PositionUserDB {
+    NAME: string;
+    POSITION_X: number;
+    POSITION_Y: number;
+}
